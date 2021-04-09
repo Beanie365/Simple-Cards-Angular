1cafb427b37c916bf61ae007bef90f17bcc89e1d
Amiena Allie <alliea098@gmail.com>
2021-04-09 17:51:31 +0200

    Simple Angular card displayer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cards';
+export class AppComponent {s
+  posts = [
+{title: 'Neat Tree',
+imageURL: 'assets/tree.jpeg',
+username: '@nature',
+content: 'I saw this neat tree today'
+},
+
+{title: 'Snowy Mountain',
+imageURL: 'assets/mountain.jpeg',
+username: '@mountainlover',
+content: 'here is a picture of a snowy mountain'
+},
+
+{title: 'Mountain Biking',
+imageURL: 'assets/biking.jpeg',
+username: '@biking2222',
+content: 'I did some biking today'
+}
+
+  ]
 }
